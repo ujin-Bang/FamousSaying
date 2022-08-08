88998525dffc6c4bca717a1bcfe45c969f5b8b69
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-08 17:51:01 +0900

    명언 -> 뷰페이저2 배치 + 페이지 1장 xml 그리기

--- app/src/main/java/com/restart/famoussaying/MainActivity.kt
+++ app/src/main/java/com/restart/famoussaying/MainActivity.kt
@@ -2,10 +2,14 @@ package com.restart.famoussaying
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
