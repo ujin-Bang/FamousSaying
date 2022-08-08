6c231adc2f5c6b3e31fc0144b08c059abaaaa53e
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-08 18:42:34 +0900

    메인에 배치된 뷰페이저2와 만들어 놓은 어댑터 연결

--- app/src/main/java/com/restart/famoussaying/MainActivity.kt
+++ app/src/main/java/com/restart/famoussaying/MainActivity.kt
@@ -3,13 +3,31 @@ package com.restart.famoussaying
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager2.widget.ViewPager2
+import com.restart.famoussaying.adapters.QuotesPagerAdapter
+import com.restart.famoussaying.data.Quote
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initView()
+
+    }
+
+    private fun initView() {
+        viewPager.adapter = QuotesPagerAdapter(
+            listOf(
+                Quote("나는 생각한다. 고로나느 존재한다.", "데카르트")
+            )
+        )
     }
 
 }
